04a6fc95a131befc735c1b6155ead4c59ec82678
Yjuhu <jakov.sarolic.1@gmail.com>
2024-11-23 04:49:22 +0100

    Admin privileges and functions granted.

--- combis_interview/comapp/admin.py
+++ combis_interview/comapp/admin.py
@@ -4,4 +4,6 @@ from .models import Device
 @admin.register(Device)
 class DeviceAdmin(admin.ModelAdmin):
     list_display = ('device_id', 'hostname', 'ip_address', 'status', 'location')
-# Register your models here.
+    search_fields = ('device_id', 'hostname', 'ip_address', 'location')
+    list_filter = ('status', 'location')
+
